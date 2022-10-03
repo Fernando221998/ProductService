3034c23673a695209cec705e8a46f118746b2d8e
Fernando221998 <94843540+Fernando221998@users.noreply.github.com>
2022-10-03 20:41:02 +0200

    Add files via upload

---
+++ producservice/src/main/java/com/nttdata/boocamp/producservice/ProducserviceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.boocamp.producservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducserviceApplication.class, args);
+	}
+
+}
